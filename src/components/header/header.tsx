30a955ddccc880e4894a9f86d3a5300dfac74ac7
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-08 00:44:12 -0300

    feat(component-header): adding base header component styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div>header</div>
+    <div className='flex items-center justify-between'>
+      <div className='flex items-center gap-5'>
+        <Link href='/' className='text-2xl font-extrabold text-white'>
+        DevStore
+        </Link>
+        <form className='flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700'>
+          <Search className='w-5 h-5 text-zinc-500'/>
+          <input type='text' placeholder='Search products...' className='flex-1 bg-transparent outline-none text-sm placeholder:text-zinc-500'/>
+        </form>
+      </div>
+     
+      <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-2'>
+          <ShoppingBag className='w-4 h-4' />
+          <span className='text-sm'>Cart: (0)</span>
+        </div>   
+        <div className='w-px h-4 bg-zinc-700'/>
+        <Link href='/' className='flex items-center gap-2 hover:underline'>
+          <span className='text-sm'>Account</span>
+          <Image src='https://github.com/lucasyule2212.png' alt='' className='w-6 h-6 rounded-full' width={24} height={24} />
+        </Link>
+      </div>
+    </div>
   )
 }
