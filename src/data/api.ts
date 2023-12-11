ca912ea275a88ff51ba1fe0cd04fad717a2c5aaf
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-11 11:07:04 -0300

    refactor(data-api): changing init param to non-obrigatory

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 
-export default function api({ path, init }: { path: string, init:RequestInit }) {
+export default function api({ path, init }: { path: string, init?:RequestInit }) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL;
   const apiPrefix = '/api'
   const url = new URL(apiPrefix.concat(path), baseUrl);
