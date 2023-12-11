31715c01a79ebf40198658afb09e08c477544da5
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-11 14:25:16 -0300

    refactor(api-products-featured): adding dummy loading timeout to simulate realistic api call

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,5 +1,6 @@
-import data from '../data.json'
+import data from '../data.json';
 export async function GET() {
-  const featuredProducts = data.products.filter(product => product.featured)
-  return Response.json(featuredProducts)
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const featuredProducts = data.products.filter((product) => product.featured);
+  return Response.json(featuredProducts);
 }
