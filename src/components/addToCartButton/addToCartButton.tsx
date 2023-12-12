2ec43dd4209d53b3a16e387a1be8b72671e13c50
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-12 11:13:28 -0300

    feat(components-addtocartbutton): adding base AddToCartButton component

---
+++ src/components/addToCartButton/addToCartButton.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function AddToCartButton() {
+  return (
+    <button
+      type="button"
+      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white transition-colors hover:bg-emerald-700"
+    >
+      Adicionar ao carrinho
+    </button>
+  );
+}
