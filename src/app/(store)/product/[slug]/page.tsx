7dccff1275cb7ffd9cb82b0acce9b0e66a50eca0
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-12 11:43:04 -0300

    refactor(store-product-page): adding product data request by slug + fetched product info in the page

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,14 +1,40 @@
 import AddToCartButton from '@/components/addToCartButton/addToCartButton';
 import ProductSizeButton from '@/components/productSizeButton/productSizeButton';
+import api from '@/data/api';
+import { type Product } from '@/data/types/product';
 import Image from 'next/image';
+interface ProductPageProps {
+  params: {
+    slug: string;
+  };
+}
 
-export default function ProductPage() {
+async function getProductBySlug({ slug }: { slug: string }): Promise<Product> {
+  const response = await api({
+    path: `/products/${slug}`,
+    init: {
+      next: {
+        revalidate: 60 * 60, // 1 hour
+      },
+    },
+  });
+
+  const product = (await response.json()) as Product;
+
+  return product;
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const product = await getProductBySlug({
+    slug: params.slug,
+  });
+
   return (
     <div className="relative grid max-h-[860vh] grid-cols-3">
       <div className="col-span-2 flex  justify-center overflow-hidden">
         <Image
           className="relative"
-          src="/moletom-never-stop-learning.png"
+          src={product?.image ?? ''}
           alt=""
           width={610}
           height={610}
@@ -17,17 +43,27 @@ export default function ProductPage() {
       </div>
       <div className="flex flex-col justify-center px-12">
         <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
+          {product?.title ?? ''}
         </h1>
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster.{' '}
+          {product.description ?? ''}
         </p>
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$ 129
+            {product.price?.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL',
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x sem juros de 10,75
+            Em 12x sem juros de{' '}
+            {(product.price / 12).toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL',
+              maximumFractionDigits: 2,
+            })}
           </span>
         </div>
         <div className="mt-8 space-y-4 ">
