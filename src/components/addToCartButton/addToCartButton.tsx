21ee95c89b2a9b8af96dd92de0f6cf7ac3b3d0f8
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-13 21:24:42 -0300

    refactor(addtocartbutton): adding funcionalities + style behavior

--- src/components/addToCartButton/addToCartButton.tsx
+++ src/components/addToCartButton/addToCartButton.tsx
@@ -1,12 +1,53 @@
 'use client';
 
-export default function AddToCartButton() {
+import { useToast } from '@/components/ui/use-toast';
+import { useCart } from '@/contexts/cartContext';
+import { useMemo } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export default function AddToCartButton({ productId }: { productId: number }) {
+  const { addToCart, removeFromCart, cartItems, selectedSize } = useCart();
+  const itemIsInCart = useMemo(
+    () => cartItems.some((item) => item.productId === productId),
+    [cartItems],
+  );
+  const { toast } = useToast();
+
+  const buttonCollor = useMemo(
+    () =>
+      itemIsInCart
+        ? 'bg-violet-600 hover:bg-violet-700'
+        : 'bg-emerald-600 hover:bg-emerald-700',
+    [itemIsInCart],
+  );
+
+  const handleAddToCart = () => {
+    if (selectedSize === '') {
+      toast({
+        variant: 'destructive',
+        description: 'Você precisa selecionar um tamanho para continuar',
+        duration: 5000,
+      });
+      return;
+    }
+
+    if (itemIsInCart) {
+      removeFromCart(productId);
+    } else {
+      addToCart(productId);
+    }
+  };
+
   return (
     <button
       type="button"
-      className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white transition-colors hover:bg-emerald-700"
+      className={twMerge(
+        'mt-8 flex h-12 items-center justify-center rounded-full font-semibold text-white transition-colors',
+        buttonCollor,
+      )}
+      onClick={() => handleAddToCart()}
     >
-      Adicionar ao carrinho
+      {itemIsInCart ? 'Remover do carrinho' : 'Adicionar ao carrinho'}
     </button>
   );
 }
