d10d5486c39ccc667818551d308fb0677d3813cc
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-13 21:23:45 -0300

    refactor(productsizebutton): adding funcionalities + style behavior

--- src/components/productSizeButton/productSizeButton.tsx
+++ src/components/productSizeButton/productSizeButton.tsx
@@ -1,10 +1,27 @@
 'use client';
 
+import { useCart } from '@/contexts/cartContext';
+import { useMemo } from 'react';
+import { twMerge } from 'tailwind-merge';
+
 export default function ProductSizeButton({ size }: { size: string }) {
+  const { selectedSize, setSelectedSize } = useCart();
+
+  const selectedButtonColor = useMemo(() => {
+    if (selectedSize === size) {
+      return 'border-violet-500 text-violet-500';
+    }
+    return 'border-zinc-600';
+  }, [selectedSize]);
+
   return (
     <button
       type="button"
-      className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold transition-colors hover:bg-zinc-900"
+      className={twMerge(
+        'flex h-9 w-14 items-center justify-center rounded-full border  bg-zinc-800 text-sm font-semibold transition-colors hover:bg-zinc-900',
+        selectedButtonColor,
+      )}
+      onClick={() => setSelectedSize(size)}
     >
       {size}
     </button>
