3f2aaa24789592af702ab086fa4cf14950dc68f2
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-24 11:39:15 -0300

    refactor(store-search-loading): adding skeleton layout to search page loading fallback

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+'use client';
+import Skeleton from '@/components/skeleton/skeleton';
+import { useSearchParams } from 'next/navigation';
+
 export default function SearchLoading() {
-  return <div>Carregando busca...</div>;
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q');
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Results to: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[280px]" />
+        <Skeleton className="h-[280px]" />
+        <Skeleton className="h-[280px]" />
+        <Skeleton className="h-[280px]" />
+        <Skeleton className="h-[280px]" />
+        <Skeleton className="h-[280px]" />
+      </div>
+    </div>
+  );
 }
