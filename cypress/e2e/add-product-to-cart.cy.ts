607aed0bb888bd17c0a552ea7972f01d1e7b6a76
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-30 21:01:38 -0300

    feat(cypress/commands): add custom searchByQuery command + using it at tests

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -34,7 +34,7 @@ describe('add product to cart', () => {
   });
 
   it('should be able to search a product and add it to the cart', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit();
+    cy.searchByQuery('moletom');
 
     cy.get('a[href^="/product"]').first().click();
 
