83ab8f0412249b3b6c01592533ad2180fb632d88
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-30 21:07:48 -0300

    feat(next.config.js): add ignoreDuringBuilds eslint and typescript errors

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    domains: ['github.com']
-  }
+    domains: ['github.com'],
+  },
 };
 
 export default config;
