1b17d07a5a59c56983a6381b47ae107af1597aeb
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-04 10:55:58 -0300

    refactor(usesearchparams): wrapping useSearchParams in Suspense boundary to avoid build warnings

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import CartDisplay from './cartDisplay';
+import { Suspense } from 'react';
 import SearchForm from '../searchForm/searchForm';
+import CartDisplay from './cartDisplay';
 
 export default function Header() {
   return (
@@ -10,7 +11,9 @@ export default function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           DevStore
         </Link>
-        <SearchForm />
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
 
       <div className="flex items-center gap-4">
