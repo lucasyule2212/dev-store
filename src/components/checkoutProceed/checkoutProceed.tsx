625635fa29f0a09a3ee32a7824c364d3ddbb62b1
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-09 00:36:23 -0300

    refactor(checkoutproceed): adding redirectCheckout handler + proceed to checkout button loading

--- src/components/checkoutProceed/checkoutProceed.tsx
+++ src/components/checkoutProceed/checkoutProceed.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 'use client';
 
 import { useCart } from '@/contexts/cartContext';
+import { env } from '@/env';
+import { useUser } from '@clerk/nextjs';
+import { loadStripe } from '@stripe/stripe-js';
 import { ShoppingBag } from 'lucide-react';
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { Button } from '../ui/button';
+import Spinner from '../ui/spinner';
 
 export default function CheckoutProceed() {
   const { cartItems } = useCart();
+  const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
 
   const cartItemsSubtotal = useMemo(() => {
     return cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
@@ -14,6 +21,40 @@ export default function CheckoutProceed() {
 
   const hasItems = useMemo(() => {
     return cartItems.length > 0;
+  }, [cartItems]);
+
+  const redirectToCheckout = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const stripe = await loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+      if (!stripe) throw new Error('Stripe failed to initialize.');
+
+      const checkoutResponse = await fetch('/api/checkout_sessions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cartDetails: cartItems.map((item) => ({
+            title: item.title,
+            quantity: item.quantity,
+            price: item.price,
+          })),
+          customerEmail: user?.emailAddresses[0]?.emailAddress,
+        }),
+      });
+
+      const { sessionId } = await checkoutResponse.json();
+      const stripeError = await stripe.redirectToCheckout({ sessionId });
+
+      if (stripeError) {
+        console.error(stripeError);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   }, [cartItems]);
 
   return (
@@ -45,10 +86,21 @@ export default function CheckoutProceed() {
         </div>
       )}
       <Button
-        disabled={!hasItems}
+        disabled={!hasItems || isLoading}
         className="mt-6 flex justify-center gap-2 justify-self-end rounded-md bg-violet-500 p-2 font-semibold text-zinc-50 hover:bg-violet-600"
+        onClick={redirectToCheckout}
       >
-        <ShoppingBag /> Proceed to Checkout
+        {isLoading ? (
+          <>
+            <Spinner />
+            Proceeding to Checkout
+          </>
+        ) : (
+          <>
+            <ShoppingBag />
+            Proceed to Checkout
+          </>
+        )}
       </Button>
     </div>
   );
