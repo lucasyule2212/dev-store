01a5c3f7f0e5becdd71e962b9c92c42f35a7d1c3
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-10 23:35:14 -0300

    refactor(cartitemlist): adding remove cart item button to cartItem

--- src/components/checkoutProceed/cartItemsList.tsx
+++ src/components/checkoutProceed/cartItemsList.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { useCart, type CartItem } from '@/contexts/cartContext';
+import { Trash } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 function CartItem({ item }: { item: CartItem }) {
+  const { removeFromCart } = useCart();
+
   return (
     <Link
       href={`/product/${item.slug}`}
@@ -12,9 +16,18 @@ function CartItem({ item }: { item: CartItem }) {
     >
       <div className="flex items-center gap-3">
         <Image src={item.image} alt="" width={150} height={150} />
-        <span className="text-xl font-bold">
-          {item.title} (<strong>{item.size}</strong>)
-        </span>
+        <div className="mt-14 flex flex-col gap-4">
+          <span className="text-xl font-bold">
+            {item.title} (<strong>{item.size}</strong>)
+          </span>
+          <Button
+            className="absolut right-10 top-10 flex w-fit gap-2 text-sm font-semibold shadow-md ring-1 ring-zinc-800 hover:bg-zinc-900 group-hover:bg-zinc-800 group-hover:ring-zinc-700"
+            size="sm"
+            onClick={() => removeFromCart(item.id)}
+          >
+            Remove <Trash width={18} height={18} />
+          </Button>
+        </div>
         <span className="text-3xl font-extrabold text-violet-500">
           {item.quantity}x
         </span>
