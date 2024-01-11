6bf4ad97715a9eef2bf50b21681bd44c0c0f9e1e
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-10 23:55:52 -0300

    feat(confirmationmodal): adding base confirmationModal component

---
+++ src/components/confirmationModal/confirmationModal.tsx
@@ -0,0 +1,44 @@
+import { Button } from '../ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog';
+
+interface ConfirmationModalProps {
+  title: string;
+  description?: string;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  onConfirm: () => void;
+}
+
+export default function ConfirmationModal({
+  title,
+  description,
+  confirmButtonText = 'Confirm',
+  cancelButtonText = 'Cancel',
+  onConfirm,
+}: ConfirmationModalProps) {
+  <Dialog>
+    <DialogContent className="sm:max-w-md">
+      <DialogHeader>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogDescription>{description}</DialogDescription>
+      </DialogHeader>
+      <DialogFooter className="sm:justify-start">
+        <DialogClose asChild>
+          <Button variant="secondary">{cancelButtonText}</Button>
+        </DialogClose>
+        <Button variant="destructive" onClick={onConfirm}>
+          {confirmButtonText}
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>;
+}
