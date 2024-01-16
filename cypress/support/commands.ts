885b5ee44a44f66a41c1f260ab0a22d25c72630a
Lucas Yule <lyrma@cin.ufpe.br>
2024-01-16 00:10:34 -0300

    feat(cypress-commands): adding base cypress signIn with Clerk mock

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /// <reference types="cypress" />
 //
 //
@@ -7,6 +8,7 @@ declare global {
   namespace Cypress {
     interface Chainable {
       searchByQuery(query: string): Chainable<void>;
+      signIn(): Chainable<void>;
     }
   }
 }
@@ -16,6 +18,47 @@ Cypress.Commands.add('searchByQuery', (query: string) => {
     failOnStatusCode: false,
   });
   cy.get('input[name=q]').type(query).parent('form').submit();
+});
+
+declare global {
+  interface Window {
+    Clerk: {
+      isReady: () => boolean;
+      client: {
+        signIn: {
+          create: (options: {
+            identifier: string;
+            password: string;
+          }) => Promise<{ createdSessionId: string }>;
+        };
+      };
+      setActive: (options: { session: string }) => Promise<void>;
+    };
+  }
+}
+
+Cypress.Commands.add('signIn', () => {
+  cy.log(`Signing in.`);
+  cy.visit(`/`);
+
+  cy.window()
+    .should((window: Window) => {
+      expect(window).to.not.have.property(`Clerk`, undefined);
+      expect(window.Clerk.isReady()).to.eq(true);
+    })
+    .then(async (window: Window) => {
+      cy.clearCookies({ domain: window.location.hostname });
+      const res = await window.Clerk.client.signIn.create({
+        identifier: Cypress.env(`test_email`),
+        password: Cypress.env(`test_password`),
+      });
+
+      await window.Clerk.setActive({
+        session: res.createdSessionId,
+      });
+
+      cy.log(`Finished Signing in.`);
+    });
 });
 //
 //
